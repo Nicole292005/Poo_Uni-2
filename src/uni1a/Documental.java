f47d3783fddd2ae732a03906fd6e1e89a8901e63
Nicole292005 <ndramosmejia@gmail.com>
2025-07-06 23:23:07 -0500

    Etapa 2 y 3

--- src/uni1a/Documental.java
+++ src/uni1a/Documental.java
@@ -3,13 +3,18 @@
  */
 package uni1a;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Subclase Documental que extiende de ContenidoAudiovisual
 public class Documental extends ContenidoAudiovisual {
     private String tema;
+    private List<Investigador> investigadores; // Association: A Documental has multiple Investigadores
 
     public Documental(String titulo, int duracionEnMinutos, String genero, String tema) {
         super(titulo, duracionEnMinutos, genero);
         this.tema = tema;
+        this.investigadores = new ArrayList<>();
     }
 
     public String getTema() {
@@ -19,15 +24,25 @@ public class Documental extends ContenidoAudiovisual {
     public void setTema(String tema) {
         this.tema = tema;
     }
-    
+
+    public void agregarInvestigador(Investigador investigador) {
+        this.investigadores.add(investigador);
+    }
+
     @Override
     public void mostrarDetalles() {
-        System.out.println("Detalles de la película:");
+        System.out.println("Detalles del Documental:");
         System.out.println("ID: " + getId());
         System.out.println("Título: " + getTitulo());
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Tema: " + this.tema);
+        if (!investigadores.isEmpty()) {
+            System.out.println("Investigadores Principales:");
+            for (Investigador inv : investigadores) {
+                inv.mostrarDetalles();
+            }
+        }
         System.out.println();
     }
 }
